53b2280dc886e05b853aa50ae73ebe3803a251a3
bach.ngoc.hoai <bach.ngoc.hoai@Bach-Ngoc-Hoai.local>
2024-10-29 16:09:00 +0700

    Update UT

--- domain/service/article_impl_test.go
+++ domain/service/article_impl_test.go
@@ -6,6 +6,7 @@ import (
 	"echo-clean/domain/repository/mocks"
 	"github.com/go-faker/faker/v4"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"testing"
 )
 
@@ -46,12 +47,77 @@ func TestFetchArticle(t *testing.T) {
 	})
 }
 
+func TestStore(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		mockArticleRepository, mockAuthorRepository, articleServiceImpl := setup()
+
+		// Fake new article
+		var mockArticle entity.Article
+		var mockAuthor entity.Author
+		_ = faker.FakeData(&mockArticle)
+		_ = faker.FakeData(&mockAuthor)
+
+		mockListAuthor := make([]*entity.Author, 0)
+		mockListAuthor = append(mockListAuthor, &mockAuthor)
+
+		// Given data for Article Repository Mock
+		mockAuthorRepository.On("Fetch").Return(mockListAuthor, nil)
+		mockArticleRepository.On("Store", mock.Anything, mock.Anything).Return(&mockArticle, nil)
+
+		// When: Call Store function
+		result, e := articleServiceImpl.Store(&mockArticle)
+
+		// Then: Assert the result
+		assert.Empty(t, e, nil)
+		assert.Equal(t, result, &mockArticle)
+	})
+
+	t.Run("error-validation", func(t *testing.T) {
+		_, _, articleServiceImpl := setup()
+
+		// Fake new article
+		mockArticle := entity.Article{
+			Title:   "",
+			Content: "",
+		}
+
+		// When: Call Store function
+		result, e := articleServiceImpl.Store(&mockArticle)
+
+		// Then: Assert the result
+		assert.Empty(t, result)
+		assert.Equal(t, e, []serviceErrors.Error{serviceErrors.StatusBadRequestError})
+	})
+
+	t.Run("error-failed", func(t *testing.T) {
+		mockArticleRepository, mockAuthorRepository, articleServiceImpl := setup()
+
+		// Fake new article
+		var mockArticle entity.Article
+		var mockAuthor entity.Author
+		_ = faker.FakeData(&mockArticle)
+		_ = faker.FakeData(&mockAuthor)
+
+		mockListAuthor := make([]*entity.Author, 0)
+		mockListAuthor = append(mockListAuthor, &mockAuthor)
+
+		// Given data for Article Repository Mock
+		mockAuthorRepository.On("Fetch").Return(mockListAuthor, nil)
+		mockArticleRepository.On("Store", mock.Anything, mock.Anything).Return(nil, assert.AnError)
+
+		// When: Call Store function
+		result, e := articleServiceImpl.Store(&mockArticle)
+
+		// Then: Assert the result
+		assert.Empty(t, result)
+		assert.Equal(t, e, []serviceErrors.Error{serviceErrors.E10001})
+	})
+}
+
 func setup() (*mocks.ArticleRepository, *mocks.AuthorRepository, ArticleService) {
-	// Tạo mock repository mới cho mỗi test case
 	mockArticleRepository := new(mocks.ArticleRepository)
 	mockAuthorRepository := new(mocks.AuthorRepository)
 
-	// Khởi tạo service với mock
 	articleServiceImpl := NewArticleService(mockArticleRepository, mockAuthorRepository)
 
 	return mockArticleRepository, mockAuthorRepository, articleServiceImpl
