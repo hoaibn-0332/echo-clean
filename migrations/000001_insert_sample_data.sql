10bb95c1088beb21af57b60911b0faab01e2d0c5
bach.ngoc.hoai <bach.ngoc.hoai@Bach-Ngoc-Hoai.local>
2024-10-30 16:43:26 +0700

    Update docker

---
+++ migrations/000001_insert_sample_data.sql
@@ -0,0 +1,9 @@
+INSERT INTO author (name)
+VALUES ('Author 2'),
+       ('Author 3'),
+       ('Author 4');
+
+INSERT INTO article (title, content, author_id)
+VALUES ('Title 2', 'Content of the first article', 2),
+       ('Title 3', 'Content of the second article', 3),
+       ('Title 4', 'Content of the third article', 4);
