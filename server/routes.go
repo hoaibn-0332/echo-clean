001dabbce749563fd34690835060cdab4c042b13
bach.ngoc.hoai <bach.ngoc.hoai@sun-asterisk.com>
2024-10-31 00:32:54 +0700

    Create handlers struct

--- server/routes.go
+++ server/routes.go
@@ -1,13 +1,12 @@
 package server
 
 import (
-	"echo-clean/server/handler"
 	"echo-clean/server/route"
 	"github.com/labstack/echo/v4"
 )
 
-func RegisterRoutes(e *echo.Echo, article *handler.ArticleHandler) {
-	route.RegisterArticleRoutes(e, article)
+func RegisterRoutes(e *echo.Echo, handlers *Handlers) {
+	route.RegisterArticleRoutes(e, handlers.Article)
 
 	// Health check the server
 	e.GET("/health", HealthCheck)
